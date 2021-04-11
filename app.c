174bf77f90056ed309114158b5598a558df8a45f
eecheng <yucheng871011@gmail.com>
2021-04-11 16:14:07 +0800

    Allow to get cpu cycle in app

--- app.c
+++ app.c
@@ -11,7 +11,11 @@ int main() {
     /* warm-up */
     for(i = 0; i < 80000000; i++);
     long long a, b;
+#if defined(__x86_64__)
     a = __rdtsc();
+#elif defined(__aarch64__)
+    asm volatile("mrs %0, pmccntr_el0" : "=r"(a));
+#endif
     syscall(401);
     syscall(401);
     syscall(401);
@@ -132,7 +136,11 @@ int main() {
     syscall(401);
     syscall(401);
     syscall(401);
+#if defined(__x86_64__)
     b = __rdtsc();
+#elif defined(__aarch64__)
+    asm volatile("mrs %0, pmccntr_el0" : "=r"(b));
+#endif
     printf("%lld \n", (b - a) / 120);
     return 0;
 }
